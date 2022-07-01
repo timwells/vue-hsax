11d83407ff124f77f56a627d593c5768701fea2d
zx25 <timwells018@gmail.com>
2022-07-01 23:56:36 +0100

    express base cloud fn

---
+++ functions/config_template.js
@@ -0,0 +1,14 @@
+
+// Rename config_template.js to config.js
+// Add api_keys to array as guuid's
+const config = {
+    apiKeys: [
+        "aaaaaaaa-1111-2222-3333-xxxxxxxxxxxx",
+        "bbbbbbbb-1111-2222-3333-yyyyyyyyyyyy",
+        "cccccccc-1111-2222-3333-zzzzzzzzzzzz",
+    ]
+}
+
+module.exports = {
+    config
+}
--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,41 @@
 const functions = require("firebase-functions");
+const admin = require("firebase-admin");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+
+const axios = require('axios');
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const { config } = require("./config")
+
+const VERSION = "0.0.1";
+const API_KEY_NAME = "x-api-key"
+
+const isApiKeyValid = (request,keyName,apiKeys) => {
+    const apiKey = request.header(keyName);
+    return (apiKey != undefined && apiKey != null && apiKey.length > 0) ? apiKeys.includes(apiKey) : false;
+}
+
+// Automatically allow cross-origin requests
+app.use(cors({ origin: true }));
+
+app.get('/version', (request, response) => {
+    response.send(VERSION);
+})
+
+app.get('/version-secured', (request, response) => {
+  if(isApiKeyValid(request,API_KEY_NAME,config.apiKeys)) {
+    response.status(200).send(VERSION);
+  } else {
+    response.status(401).send('unauthorized');
+  }
+})
+
+// Expose Express API as a single Cloud Function:
+exports.hsax = functions.https.onRequest(app);
+
+
+
+
+
